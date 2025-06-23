05a1d54601e20a8825e217427bfb901336ea122d
Derek Hunter <lash209@gmail.com>
2025-06-22 22:01:03 -0700

    Convert to Action Inputs rather than the baked in UI events

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,31 +8,30 @@ const SPEED := 200.0
 var last_direction := "down"
 
 func _physics_process(_delta):
-    var input_vector := Vector2(
-        Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-        Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-    )
+	var input_vector := Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	)
 
-    if input_vector != Vector2.ZERO:
-        input_vector = input_vector.normalized()
-        velocity = input_vector * SPEED
+	if input_vector != Vector2.ZERO:
+		input_vector = input_vector.normalized()
+		velocity = input_vector * SPEED
 
-        # Animation logic: left/right takes priority
-        if input_vector.x > 0:
-            anim_sprite.play("walk_right")
-            last_direction = "right"
-        elif input_vector.x < 0:
-            anim_sprite.play("walk_left")
-            last_direction = "left"
-        elif input_vector.y > 0:
-            anim_sprite.play("walk_down")
-            last_direction = "down"
-        elif input_vector.y < 0:
-            anim_sprite.play("walk_up")
-            last_direction = "up"
-    else:
-        velocity = Vector2.ZERO
-        anim_sprite.play("face_%s" % last_direction)
+		# Animation logic: left/right takes priority
+		if input_vector.x > 0:
+			anim_sprite.play("walk_right")
+			last_direction = "right"
+		elif input_vector.x < 0:
+			anim_sprite.play("walk_left")
+			last_direction = "left"
+		elif input_vector.y > 0:
+			anim_sprite.play("walk_down")
+			last_direction = "down"
+		elif input_vector.y < 0:
+			anim_sprite.play("walk_up")
+			last_direction = "up"
+	else:
+		velocity = Vector2.ZERO
+		anim_sprite.play("face_%s" % last_direction)
 
-    move_and_slide()
-
+	move_and_slide()
