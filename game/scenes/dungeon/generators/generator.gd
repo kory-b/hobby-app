ec50a2a48e9902dfa22d952ec0b8aa4c545a5824
Derek Hunter <lash209@gmail.com>
2025-07-03 13:50:10 -0700

    Pulling the dungeon geneator out of the dungeon manager

--- game/scenes/dungeon/generators/generator.gd
+++ game/scenes/dungeon/generators/generator.gd
@@ -1,1 +1,11 @@
-extends Node
+class_name Generator extends Node
+
+var player_spawn: Vector2i
+var rooms: Array
+var exit_spawn: Vector2i
+
+func generate(floor_layer: TileMapLayer, wall_layer: TileMapLayer) -> void:
+	pass
+
+func get_player_Spawn():
+	pass
