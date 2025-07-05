0f98f97e0e77931778c35ab398f221e8d86cc302
Derek Hunter <lash209@gmail.com>
2025-07-04 21:10:21 -0700

    Rebasing

--- game/ui/health_bar/health_bar.gd
+++ game/ui/health_bar/health_bar.gd
@@ -1,16 +1,11 @@
 extends ProgressBar
 
-@export var health_component: HealthComponent
-
-
-func _ready() -> void:
-	init_value(health_component.max_health)
-
 func init_value(value: int) -> void:
 	min_value = 0
 	value = value
 	max_value = value
 
-func _on_health_component_health_changed(new_health: Variant) -> void:
+
+func _on_current_stats_health_changed(new_health: Variant) -> void:
 	print("Updating Health Component: ", new_health)
 	value = new_health
